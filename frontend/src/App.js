e3eb422531c53c77e9e84a53bdaf71be9ccec22c
Tri Nguyen <tri09t4@gmail.com>
2021-08-20 19:25:05 +0700

    12.8. Front End Image Upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import OrderPage from './pages/OrderPage';
 import UserListPage from './pages/UserListPage';
 import UserEditPage from './pages/UserEditPage';
 import ProductListPage from './pages/ProductListPage';
+import ProductEditPage from './pages/ProductEditPage';
 
 
 const App = () => {
@@ -36,6 +37,7 @@ const App = () => {
           <Route path='/admin/userlist' component={UserListPage} />
           <Route path='/admin/users/:id/edit' component={UserEditPage} />
           <Route path='/admin/productlist' component={ProductListPage} />
+          <Route path='/admin/product/:id/edit' component={ProductEditPage} />
         </Container>
       </main>
       <Footer />
